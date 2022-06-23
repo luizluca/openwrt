4549db8e18404dacd71d5c88f1714010521b3aa4
Luiz Angelo Daros de Luca <luizluca@gmail.com>
2022-06-23 17:50:03 -0300

    realtek: add DGS-1210-28 factory image
    DGS-1210 switches support dual image, with each image composed of a
    kernel and a rootfs partition. For image1, kernel and rootfs are in
    sequence. The current OpenWrt image (written using a serial console),
    uses those partitions together as the firmware partition, ignoring the
    partition division. The current OEM u-boot fails to validate image1 but
    it will only trigger firmware recovery if both image1 and image2 fail,
    and it does not switch the boot image in case one of them fails the
    check.
    
    The OEM factory image is composed of concatenated blocks of data, each
    one prefixed with a 0x40-byte cameo header. A normal OEM firmware will
    have two of these blocks (kernel, rootfs). The OEM firmware only checks
    the header before writing unconditionally the data (except the header)
    to the correspoding partition.
    
    The OpenWrt factory image mimics the OEM image by cutting the
    kernel+rootfs firmware at the exact size of the OEM kernel partition
    and packing it as "the kernel partition" and the rest of the kernel and
    the rootfs as "the rootfs partition". It will only work if written to
    image1 because image2 has a sysinfo partition between kernel2 and
    rootfs2, cutting the kernel code in the middle.
    
    Steps to install:
    
    1) switch to image2 (containing an OEM image), using web or these CLI
       commands:
       - config firmware image_id 2 boot_up
       - reboot
    2) flash the factory_only_for_image1.bin to image1. OEM web (v6.30.016)
       is crashing for any upload (ssh keys, firmware), even applying OEM
       firmwares. These CLI commands can upload a new firmware to the other
       image location (not used to boot):
       - download firmware_fromTFTP <tftpserver> factory_only_for_image1.bin
       - config firmware image_id 1 boot_up
       - reboot
    
    Signed-off-by: Luiz Angelo Daros de Luca <luizluca@gmail.com>

--- target/linux/realtek/image/rtl838x.mk
+++ target/linux/realtek/image/rtl838x.mk
@@ -33,6 +33,14 @@ TARGET_DEVICES += d-link_dgs-1210-16
 define Device/d-link_dgs-1210-28
   $(Device/d-link_dgs-1210)
   DEVICE_MODEL := DGS-1210-28
+  DLINK_KERNEL_PART_SIZE := 1572864
+  CAMEO_KERNEL_PART := 2
+  CAMEO_ROOTFS_PART := 3
+  CAMEO_CUSTOMER_SIGNATURE := 2
+  CAMEO_BOARD_VERSION := 32
+  IMAGES += factory_only_for_image1.bin
+  IMAGE/factory_only_for_image1.bin := append-kernel | pad-to 64k | \
+        append-rootfs | pad-rootfs | pad-to 16 | check-size | dlink-headers
 endef
 TARGET_DEVICES += d-link_dgs-1210-28
 
