9d54f0086a838328306c3704cc0d554f4e0462ed
Luiz Angelo Daros de Luca <luizluca@gmail.com>
2023-06-12 15:46:23 -0300

    ath79: convert TP-Link TL-WRD3600,TL-WRD4300 to DSA
    Both normal and failsafe are working as expected.
    tl-wdr4300-v1-il was migrated but not tested in real HW.
    
    WAN/LAN leds are now visible in sysfs but left unconfigured as it was
    not possible to reproduce the default behavior configuring sysfs.
    
    The link indication is not working together with rx/tx but it works if used
    alone. The netdev also fails to monitor other device than the default
    one. Setting interval is failing with "Invalid argument", visible when
    led service is started (including during boot).
    
    Signed-off-by: Luiz Angelo Daros de Luca <luizluca@gmail.com>

--- target/linux/ath79/image/generic-tp-link.mk
+++ target/linux/ath79/image/generic-tp-link.mk
@@ -626,7 +626,8 @@ define Device/tplink_tl-wdr3600-v1
   SOC := ar9344
   DEVICE_MODEL := TL-WDR3600
   DEVICE_VARIANT := v1
-  DEVICE_PACKAGES := kmod-usb2 kmod-usb-ledtrig-usbport -kmod-dsa-qca8k
+  DEVICE_PACKAGES := kmod-usb2 kmod-usb-ledtrig-usbport kmod-dsa-qca8k \
+	-kmod-switch-ip17xx -kmod-switch-ar8xxx -swconfig -kmod-swconfig
   TPLINK_HWID := 0x36000001
   SUPPORTED_DEVICES += tl-wdr4300
 endef
@@ -637,9 +638,12 @@ define Device/tplink_tl-wdr4300-v1
   SOC := ar9344
   DEVICE_MODEL := TL-WDR4300
   DEVICE_VARIANT := v1
-  DEVICE_PACKAGES := kmod-usb2 kmod-usb-ledtrig-usbport -kmod-dsa-qca8k
+  DEVICE_PACKAGES := kmod-usb2 kmod-usb-ledtrig-usbport kmod-dsa-qca8k \
+	-kmod-switch-ip17xx -kmod-switch-ar8xxx -swconfig -kmod-swconfig
   TPLINK_HWID := 0x43000001
   SUPPORTED_DEVICES += tl-wdr4300
+  DEVICE_COMPAT_VERSION := 1.1
+  DEVICE_COMPAT_MESSAGE := Config cannot be migrated from swconfig to DSA
 endef
 TARGET_DEVICES += tplink_tl-wdr4300-v1
 
@@ -648,7 +652,8 @@ define Device/tplink_tl-wdr4300-v1-il
   SOC := ar9344
   DEVICE_MODEL := TL-WDR4300
   DEVICE_VARIANT := v1 (IL)
-  DEVICE_PACKAGES := kmod-usb2 kmod-usb-ledtrig-usbport -kmod-dsa-qca8k
+  DEVICE_PACKAGES := kmod-usb2 kmod-usb-ledtrig-usbport kmod-dsa-qca8k \
+	-kmod-switch-ip17xx -kmod-switch-ar8xxx -swconfig -kmod-swconfig
   TPLINK_HWID := 0x43008001
   SUPPORTED_DEVICES += tl-wdr4300
 endef
@@ -662,6 +667,8 @@ define Device/tplink_tl-wdr4310-v1
   DEVICE_PACKAGES := kmod-usb2 kmod-usb-ledtrig-usbport -kmod-dsa-qca8k
   TPLINK_HWID := 0x43100001
   SUPPORTED_DEVICES += tl-wdr4300
+  DEVICE_COMPAT_VERSION := 1.1
+  DEVICE_COMPAT_MESSAGE := Config cannot be migrated from swconfig to DSA
 endef
 TARGET_DEVICES += tplink_tl-wdr4310-v1
 
